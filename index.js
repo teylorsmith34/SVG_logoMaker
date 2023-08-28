e26c4ace5444c25b983d3bbd71ff39cf156ad758
teylorsmith34 <teylorsmith34@gmail.com>
2023-08-28 17:31:33 -0400

    fixed mispelled js page and connnected shapes

--- index.js
+++ index.js
@@ -1,63 +1,78 @@
 const inquirer = require("inquirer");
-const { writeFile } = require("fs");
+const fs = require("fs");
+// const { Circle, Square, Triangle } = require("./lib/shape.js");
 const Circle = require("./lib/circle");
-const Triangle = require("./lib/triangle");
 const Square = require("./lib/square");
+const Triangle = require("./lib/triangle");
 
 async function getUserInput() {
   try {
-    const userInput = await inquirer
-      .prompt([
-        {
-          type: "input",
-          name: "text",
-          message: "Enter up to three characters for the logo:",
-          validate: (input) => {
-            return input.length <= 3;
-          },
+    const userInput = await inquirer.prompt([
+      {
+        type: "input",
+        name: "text",
+        message: "Enter up to three characters for the logo:",
+        validate: (input) => {
+          return input.length <= 3;
         },
-        {
-          type: "input",
-          name: "textColor",
-          message: "Enter the text color (keyword or hexadecimal):",
-        },
-        {
-          type: "list",
-          name: "shape",
-          message: "Select a shape:",
-          choices: ["Circle", "Square", "Triangle"],
-        },
-        {
-          type: "input",
-          name: "shapeColor",
-          message: "Enter the shape color (keyword or hexadecimal):",
-        },
-      ])
+      },
+      {
+        type: "input",
+        name: "textColor",
+        message: "Enter the text color (keyword or hexadecimal):",
+      },
+      {
+        type: "list",
+        name: "shape",
+        message: "Select a shape:",
+        choices: ["Circle", "Square", "Triangle"],
+      },
+      {
+        type: "input",
+        name: "shapeColor",
+        message: "Enter the shape color (keyword or hexadecimal):",
+      },
+    ]);
 
-      .then((response) => {
-        // check options > choose shape > accept options for colors and letters > pass to html > write to file
-        switch (response.shape) {
-          case "Circle":
-            var shapeChoice = new Circle(response.color).render();
-            break;
-          case "Triangle":
-            var shapeChoice = new Triangle(response.color).render();
-            break;
-          case "Square":
-            var shapeChoice = new Square(response.color).render();
-        }
-
-        const newSvg = svgGen(
-          shapeChoice,
-          response.fontcolor,
-          response.letters
-        );
-
-        writeFile("./examples/logo.svg", newSvg, () => {
-          console.log("Generated logo.svg");
-        });
-      });
+    return userInput;
   } catch (error) {
-    console.log(error);
+    console.error("Error getting user input:", error);
   }
 }
+
+function generateSVG(userInput) {
+  try {
+    let shape;
+
+    switch (userInput.shape) {
+      case "Circle":
+        shape = new Circle(userInput.shapeColor);
+        break;
+      case "Square":
+        shape = new Square(userInput.shapeColor);
+        break;
+      case "Triangle":
+        shape = new Triangle(userInput.shapeColor);
+        break;
+      default:
+        console.error("Invalid shape selection.");
+        process.exit(1);
+    }
+
+    shape.setText(userInput.text);
+    const svg = shape.render();
+
+    fs.writeFileSync("logo.svg", svg);
+
+    console.log("Generated logo.svg");
+  } catch (error) {
+    console.error("Error generating SVG:", error);
+  }
+}
+
+async function main() {
+  const userInput = await getUserInput();
+  generateSVG(userInput);
+}
+
+main();
